7daf2134f39f73f11a12864983e9fd67a5142b62
Giuseppe Capizzi <g.capizzi@gmail.com>
2014-11-03 23:01:31 +0100

    Add a browser REPL with weasel

--- project.clj
+++ project.clj
@@ -6,11 +6,14 @@
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [prismatic/om-tools "0.3.6"]
-                 [om "0.8.0-alpha1"]]
+                 [om "0.8.0-alpha1"]
+                 [weasel "0.4.2"]]
 
   :source-paths ["src"]
 
-  :profiles {:dev {:plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.1.3"]]
+                   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :cljsbuild {:builds [{:id "marley"
                                          :source-paths ["src"]
                                          :compiler {:output-to "marley.js"
--- src/marley/core.cljs
+++ src/marley/core.cljs
@@ -1,9 +1,11 @@
 (ns marley.core
   (:require [om.core :as om :include-macros true]
             [om-tools.core :refer-macros [defcomponent]]
-            [om-tools.dom :as dom :include-macros true]))
+            [om-tools.dom :as dom :include-macros true]
+            [weasel.repl :as ws-repl]))
 
 (enable-console-print!)
+(ws-repl/connect "ws://localhost:9001")
 
 (def app-state (atom {:text "Hello world!"}))
 
